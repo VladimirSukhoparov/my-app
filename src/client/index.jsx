724d9a8645de33ed439ffb97c0377282c14ad87d
VladimirSukhoparov <sukhoparov.v.n@gmail.com>
2023-02-06 19:41:49 +0500

    ssr

---
+++ src/client/index.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { hydrateRoot } from "react-dom/client";
+import { Header } from "../shared/Header";
+
+
+window.addEventListener("load", () => {
+  hydrateRoot(document.getElementById('root'), <Header />);
+ 
+});
